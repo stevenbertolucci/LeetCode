9b406a23e7c9c88d8f3032be707968bb814e201b
Steven Bertolucci <steven.bertolucci@yahoo.com>
2023-08-27 20:52:52 -0700

    Added new file

--- random_AI_generator.py
+++ random_AI_generator.py
@@ -1,6 +1,6 @@
 # Author: Steven Bertolucci
 # Date: 8.27.23
-# File Description: This file generates random list of 30 elements (any positive integers from 1-100 and then the
+# File Description: This file generates random list of 100 elements (any positive integers from 1-100 and then the
 #                   program will then choose a random number between 1-8 that will be used to multiply the value of the
 #                   index.
 
